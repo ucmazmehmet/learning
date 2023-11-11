bb276daca8626cbc8660ee739e4a70d2260ef84a
mucmaz1 <mucmaz1@gmail.com>
2023-11-11 20:35:12 +0300

    ekleme

---
+++ lib/araba_main.dart
@@ -0,0 +1,50 @@
+import 'package:first/araba.dart';
+
+void main(){
+
+  var bmw = Araba();
+
+  //Değer atama
+  bmw.renk = "Mavi";
+  bmw.hiz = 200;
+  bmw.calisiyormu = true;
+
+  //Değer okuma
+
+  String gelenRenk = bmw.renk;
+  print(gelenRenk);
+  print(bmw.hiz);
+  print(bmw.calisiyormu);
+
+  bmw.renk ="Kırmızı";
+  print(bmw.renk);
+
+  bmw.bilgiAl();
+
+  bmw.durdur();
+  bmw.bilgiAl();
+
+  bmw.calistir();
+  bmw.bilgiAl();
+
+  bmw.hizlan(50);
+  bmw.bilgiAl();
+
+  bmw.yavasla(30);
+  bmw.bilgiAl();
+
+  var limuzin = Araba();
+
+  limuzin.renk = "Beyaz";
+  limuzin.hiz =100;
+  limuzin.calisiyormu = true;
+
+  limuzin.bilgiAl();
+
+  print(limuzin.renk);
+
+  limuzin.durdur();
+  limuzin.bilgiAl();
+  
+
+}
